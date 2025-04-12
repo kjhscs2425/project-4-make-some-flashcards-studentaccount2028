3c2295298d8e3014caa35fcd020359c6054aa4b8
studentaccount2028 <tpark2028@kehillahstudent.org>
2025-04-12 11:51:40 -0700

    changes

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+#We need to fix historical.txt to tell the score of historical data
+#If we have time create a log in system
+
 # Write your code here
 import random
 
 #Opening Statement
-print("Welcome to the flashcard game! This game is a simple composition of difficult riddles. All riddles that you do not complete will be redirected into the next cycle. Your statistics and file data will be stored and available for your viewing. Good luck!")
+print("Welcome to the flashcard game! This game is a simple composition of difficult riddles. All riddles that you do not complete will be redirected into the next cycle. Your statistics and file data will be stored and available for your viewing. Good luck! \n")
 
 #3 different flashcard sets to randomize order
 flashcards = {
@@ -30,60 +33,70 @@ flashcards = {
     "What can travel all around the world without leaving its corner?": "A stamp"
 }
 
-false_questions = []
-f = open('stats.txt', 'r')
-g = open('historical.txt', 'a')
-readfile = f.readlines()
-f.close()
-print(readfile[-1] + " was your score last round")
-f = open('stats.txt', 'w+')
-for item in readfile:
-    if item[0] == "F":
-        false_questions.append(item[2:-1])
-print(false_questions)
+while True:
 
-flashcard_keys = list(flashcards.keys())
-random.shuffle(flashcard_keys)
-
-#Finds if the user input is correct or incorrect and has a score at the end
-correct_score = 0
-incorrect_score = 0
-
-user_question = input("Hi user! Would you like to be shown only the flashcards you haven't completed? Please respond with either a (t) for True or a (f) for False")
-if user_question == "t":
-    to_use = false_questions
-else:
-    to_use = flashcard_keys
-for key in to_use:
-    user_answer = input(key)
-    user_answer = user_answer.lower()
-    correct = flashcards[key]
-    correct = correct.lower()
-    if user_answer in correct:
-        print("Correct ✅")
-        correct_score+=1
-        
-        f.write("C|" + key + "\n")
+    #Difference between stats.txt and historical.txt is that stats.txt only tracks from the last round. Historical.txt tracks historically but only tracks write vs wrong statistic
+    false_questions = []
+    f = open('stats.txt', 'r')
+    g = open('historical.txt', 'a')
+    readfile = f.readlines()
+    f.close()
+    if len(readfile) > 0:
+        print(readfile[-1] + " was your score last round")
     else:
-        print("Incorrect ❎")
-        incorrect_score += 1
-        f.write("F|" + key + "\n")
-    print("Next Question! ")
+        print("Last rounds score could not be found \n")
+    f = open('stats.txt', 'w+')
+    for item in readfile:
+        if item[0] == "F":
+            false_questions.append(item[2:-1])
 
-f.write(str(correct_score))
-g.write(str(correct_score))
-g.write(" ")
-f.write(" ")
-f.write(str(incorrect_score))
-g.write(str(incorrect_score))
-g.write(" ")
+    flashcard_keys = list(flashcards.keys())
+    random.shuffle(flashcard_keys)
 
-f.close()
-g.close()
-g = open('historical.txt', 'r')
-cool_file = g.readlines()
-g.close()
-print(cool_file)
+    #Finds if the user input is correct or incorrect and has a score at the end
+    correct_score = 0
+    incorrect_score = 0
 
+    if len(readfile) > 0:
+        user_question = input("Hi user! Would you like to be shown only the flashcards you haven't completed? Please respond with either a (t) for True or a (f) for False \n")
+    else:
+        user_question = "f"
+    if user_question == "t":
+        to_use = false_questions
+    else:
+        to_use = flashcard_keys
+    for key in to_use:
+        user_answer = input(key)
+        user_answer = user_answer.lower()
+        correct = flashcards[key]
+        correct = correct.lower()
+        if user_answer in correct:
+            print("Correct ✅")
+            correct_score+=1
+            
+            f.write("C|" + key + "\n")
+        else:
+            print("Incorrect ❎")
+            print("The correct answer was: " + correct)
+            incorrect_score += 1
+            f.write("F|" + key + "\n")
+        print("Next Question! ")
 
-    
+    f.write(str(correct_score) + " correct")
+    g.write(str(correct_score) + " correct")
+    g.write(" ")
+    f.write(" ")
+    f.write(str(incorrect_score) + " incorrect")
+    g.write(str(incorrect_score) + " incorrect")
+    winrate = correct_score/len(to_use)
+    g.write(" ")
+    g.write("This rounds winrate: " + str(round(winrate*100, 1)) + "%")
+    g.write("\n")
+
+    f.close()
+    g.close()
+    g = open('historical.txt', 'r')
+    cool_file = g.read()
+    g.close()
+    print("Your historical data:")
+    print(cool_file)
